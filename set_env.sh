0bf2ec3c304941c18870b0bda19336b0199eaf99
mdelcamino <mdelcamino@hotmail.com>
2020-11-26 19:06:43 +0100

    new commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres2020!!
+export POSTGRES_HOST=caadb1.cjlo9y5vd5l9.eu-west-3.rds.amazonaws.com
+export POSTGRES_DB=caadb1
+export AWS_BUCKET=caa-s3-refactoringproyect
+export AWS_REGION=eu-west-3
+export AWS_PROFILE=default
+export JWT_SECRET=caa
+export URL=http://localhost:8100
